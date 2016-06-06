df59310de99598eb05a21d738577564d733de074
Gio Cielo <gccielo@gmail.com>
2016-06-05 17:22:41 -0700

    Add rank1 matrix decomposition test for classification

--- test/matrix_factorization/test_rank1.jl
+++ test/matrix_factorization/test_rank1.jl
@@ -19,6 +19,22 @@ fm = fmTrain(sparse(X), y, dim = dim)
 @test fm.w0 == 0
 @test fm.w == zeros(num_factors)
 @test_approx_eq_eps 0.0 fmEvaluateRMSE(fm, sparse(X), y) 1e-1
-info(fmEvaluateRMSE(fm, sparse(X), y))
+
+# Matrix constructed as the outer product of two vectors: [-1.; 1.] and [1. 1.]
+num_factors = 4
+X = [
+1. 1. 0. 0.;
+0. 0. 1. 1.;
+1. 0. 1. 0.;
+0. 1. 0. 1.
+]
+y = [-1. 1. -1. 1.]
+
+dim = (0, 0, 1)
+info("Decomposing Rank 1 Matrix")
+fm = fmTrain(sparse(X), y, dim = dim, task = :classification, alpha = 0.5)
+@test fm.w0 == 0
+@test fm.w == zeros(num_factors)
+@test_approx_eq_eps 0.0 fmEvaluateRMSE(fm, sparse(X), y) 0.5
 
 end
