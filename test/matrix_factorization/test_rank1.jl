829d8d407f9e0cc5970689b822f68e38acde0610
Gio Cielo <gccielo@gmail.com>
2016-06-06 14:31:01 -0700

    Tune rank1 matrix factorization tests

--- test/matrix_factorization/test_rank1.jl
+++ test/matrix_factorization/test_rank1.jl
@@ -11,14 +11,14 @@ X = [
 1. 0. 1. 0.;
 0. 1. 0. 1.
 ]
-y = [.1 .2 .2 .4]
+y = [1. 2. 2. 4.]
 
 dim = (0, 0, 1)
 info("Decomposing Rank 1 Matrix")
-fm = fmTrain(sparse(X), y, dim = dim)
+fm = fmTrain(sparse(X), y, dim = dim, alpha = 0.3)
 @test fm.w0 == 0
 @test fm.w == zeros(num_factors)
-@test_approx_eq_eps 0.0 fmEvaluateRMSE(fm, sparse(X), y) 1e-1
+@test_approx_eq_eps 0.0 fmEvaluateRMSE(fm, sparse(X), y) 1e-7
 
 # Matrix constructed as the outer product of two vectors: [-1.; 1.] and [1. 1.]
 num_factors = 4
@@ -32,7 +32,7 @@ y = [-1. 1. -1. 1.]
 
 dim = (0, 0, 1)
 info("Decomposing Rank 1 Matrix")
-fm = fmTrain(sparse(X), y, dim = dim, task = :classification, alpha = 0.5)
+fm = fmTrain(sparse(X), y, dim = dim, task = :classification, alpha = 0.3)
 @test fm.w0 == 0
 @test fm.w == zeros(num_factors)
 @test_approx_eq_eps 0.0 fmEvaluateRMSE(fm, sparse(X), y) 0.5
