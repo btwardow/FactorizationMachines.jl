26c96e748106f2a96df9d757779c5e22064933bc
Gio Cielo <gccielo@gmail.com>
2016-06-09 12:12:24 -0700

    Add missing constant on the squared error derivative and correct the corresponding tests by reducing alpha by one half

--- src/common.jl
+++ src/common.jl
@@ -8,7 +8,7 @@ export sqerr,
 
 """Squared error"""
 sqerr(p::Number, y::Number)       = (p - y)^2
-sqerr_deriv(p::Number, y::Number) = (p - y)
+sqerr_deriv(p::Number, y::Number) = 2(p - y)
 
 """Negative logistic sigmoid"""
 nlogsig(p::Number, y::Number)       = -log(sigmoid(-p * y))
