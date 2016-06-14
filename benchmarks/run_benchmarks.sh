0768c79515c0d6b497d2b510a307e63513bd7a3d
Bartlomiej Twardowski <bartlomiej.twardowski@gmail.com>
2016-06-14 18:04:06 +0200

    Automatic download 1M ML dataset for benchmarks

--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -3,9 +3,30 @@
 # Change to the directory of the current script
 cd "$(dirname "$0")"
 
+# download MovieLense datasets
+TRAIN_FILENAME="data/ml-10m.train.libfm"
+TEST_FILENAME="data/ml-10m.test.libfm"
+
+if [ ! -f "$TRAIN_FILENAME" ] || [ ! -f "$TEST_FILENAME" ]
+then 
+    echo "Downloading MovieLens 1OM dataset (6M)..."
+    curl -s -o ml-10m.zip http://files.grouplens.org/datasets/movielens/ml-10m.zip
+    unzip ml-10m.zip
+    rm ml-10m.zip
+
+    echo "Preparing train and tst files in svmlight format..."
+    mkdir data
+    python prepare_data.py ml-10M100K/ratings.dat $TRAIN_FILENAME $TEST_FILENAME
+    rm -rf ml-10M100K
+fi
+
+# simple check if fastFM package is available
+python -c 'import fastFM' || echo 'Please install fastFM package: "pip install fastFM"'
+
 echo "Running fastFM benchmark"
-time python fastfm_py_benchmark.py
+time python fastfm_py_benchmark.py $TRAIN_FILENAME $TEST_FILENAME
 echo ""
 
 echo "Running FM Julia benchmark"
-time julia fm_jl_benchmark.jl
+time julia fm_jl_benchmark.jl $TRAIN_FILENAME $TEST_FILENAME
+
