770fb2779388f89817b01cdc53328ec382d691ca
Gio Cielo <gccielo@gmail.com>
2016-08-23 17:29:34 -0700

    Fix negative log likelihood loss and its derivative and add unit tests

--- test/test_common.jl
+++ test/test_common.jl
@@ -19,4 +19,20 @@ for i in 1:length(y)
   @test expected_loss[i] == Common.heaviside(yhat[i], y[i])
 end
 
+info("Testing negative logistic sigmoid loss for {1, -1} labels")
+y =  [1.0, 1.0, -1.0, -1.0]
+yhat = [1.0, -1.0, 1.0, -1.0]
+expected_loss = [-log(Common.sigmoid(1)), log(1 + e), log(1 + e), -log(Common.sigmoid(1))]
+for i in 1:length(y)
+  @test expected_loss[i] == Common.nlogsig(yhat[i], y[i])
+end
+
+info("Testing negative logistic sigmoid loss for {1, 0} labels")
+y =  [1.0, 1.0, 0.0, 0.0]
+yhat = [1.0, 0.0, 1.0, 0.0]
+expected_loss = [-log(Common.sigmoid(1)), log(2), log(2), log(2)]
+for i in 1:length(y)
+  @test expected_loss[i] == Common.nlogsig(yhat[i], y[i])
+end
+
 end
