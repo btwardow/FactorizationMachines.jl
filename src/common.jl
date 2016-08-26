96f30b0241f03f7cfbb9d57c24593734553eadc2
Gio Borje <gccielo@gmail.com>
2016-08-26 01:55:34 -0700

    Fix negative log likelihood loss and its derivative and add unit tests (#16)

--- src/common.jl
+++ src/common.jl
@@ -11,8 +11,8 @@ sqerr(p::Number, y::Number)       = (p - y)^2
 sqerr_deriv(p::Number, y::Number) = 2(p - y)
 
 """Negative logistic sigmoid"""
-nlogsig(p::Number, y::Number)       = -log(sigmoid(-p * y))
-nlogsig_deriv(p::Number, y::Number) = y * (sigmoid(-p * y) - one(p))
+nlogsig(p::Number, y::Number)       = -log(sigmoid(p * y))
+nlogsig_deriv(p::Number, y::Number) = y * sigmoid(p * y) - y
 
 """Heaviside step function"""
 heaviside(p::Number, y::Number) = p == y ? zero(p) : one(p)
